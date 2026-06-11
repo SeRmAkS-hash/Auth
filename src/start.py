5ab0c35becc26d33ebd6c82c74c5460f1b3bf85f
Maksim <maks.samsonov02@mail.ru>
2026-06-11 12:10:17 +0300

    drop file start.py

--- src/start.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def req_to_api():
-    headers = {'authorization':'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJzdHJpbmciLCJqdGkiOiJlYjBmMTUzZC04M2MxLTQyOGEtOTIyYy05MDM0NjMxZTJkZDEiLCJ0eXBlIjoiYWNjZXNzIiwiZnJlc2giOmZhbHNlLCJjc3JmIjoiIiwiaWF0IjoxNzgwMzM1NzA4LCJleHAiOjE3ODAzMzY2MDguNjIyOTI2fQ.eJB7gG0bQ-WgT0x0i2AnudwQRUG5EdknDfPfV0fwBuU '}
-    responce = requests.get(url='http://127.0.0.1:8000/Test/endpoint',headers=headers)
-    print(responce.status_code)
-    print(responce.json())
-    
-req_to_api()
